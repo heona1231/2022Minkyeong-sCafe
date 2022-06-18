a1e4ed1f47774f3f784f99f2d83a5461490ee45b
heona <64848501+heona1231@users.noreply.github.com>
2022-06-19 00:30:48 +0900

    0619

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Mixer mixer;
+    public string[] ingredientsList = { "물", "얼음", "우유", "카카오", "커피" };
+
+    public GameObject cup;
+    public GameObject mixList;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out hit);
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.tag == "Object")
+                {
+                    int id = hit.collider.gameObject.GetComponent<ObjectInfo>().id;
+                    if (id == -1)
+                    {
+                        if (cup.activeSelf)
+                        {
+                            Debug.Log("이미 켜있음");
+                        }
+                        else
+                        {
+                            cup.SetActive(true);
+                            mixList.SetActive(true);
+                        }
+                    }
+                    else
+                    {
+                        if (cup.activeSelf)
+                        {
+                            mixer.AddIngredients(id);
+                            Debug.Log("AddIngredients" + id);
+                        }
+                        else
+                        {
+                            Debug.Log("컵켜");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void HomeButton()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
---
+++ Assets/Scripts/Mixer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Mixer : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    public int index;
+    public bool alreadyIn;
+
+    public int[] mixList;
+    public string[] ingredients;
+
+    public TextMeshProUGUI[] ingredientsUi;
+
+    void Start()
+    {
+        index = 0;
+        for (int i = 0; i < index + 1; i++)
+        {
+            mixList[i] = -1;
+            ingredientsUi[i].text = "";
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < index + 1; i++)
+        {
+            if(mixList[i] == -1)
+            {
+                ingredientsUi[i].text = "";
+            }
+            else
+            {
+                ingredientsUi[i].text = gameManager.ingredientsList[mixList[i]];
+            }
+        }
+            
+    }
+
+    public void AddIngredients(int id)
+    {
+        alreadyIn = false;
+        if (index < 4)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if(mixList[i] == id)
+                {
+                    alreadyIn = true;
+                }
+            }
+
+            if (!alreadyIn)
+            {
+                mixList[index++] = id;
+            }
+        }
+        
+        Debug.Log('s');
+    }
+}
